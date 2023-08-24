ae6cabffcae16fb899be4793a8238dddc2af471e
ThatTanishqTak <tanishqtak3@gmail.com>
2023-08-24 19:04:53 +0530

    Minor optimization

---
+++ Zombie Survival/Src/main.cpp
@@ -0,0 +1,12 @@
+// This file runs everything
+
+#include "game.h" // This file controlles everything
+
+int main()
+{
+	Game game; // Instantiate the game object
+
+	game.run(); // Call the run() function
+
+	return 0; // A good programming practice
+}
